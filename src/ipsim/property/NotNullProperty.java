579d950d87f8df1ac2b2b33a32658f7a38079d6c
Ricky <ricky@ricky-desktop.(none)>
2009-01-23 16:01:55 +0000

    Deleting some unused files

--- src/ipsim/property/NotNullProperty.java
+++
@@ -1,9 +0,0 @@
-package ipsim.property;
-
-import org.jetbrains.annotations.*;
-
-public interface NotNullProperty<T>
-{
-	void set(@NotNull T t);
-	@NotNull T get();
-}
--- src/ipsim/property/Property2.java
+++
@@ -1,9 +0,0 @@
-package ipsim.property;
-
-import org.jetbrains.annotations.*;
-
-public interface Property2<T>
-{
-	void set(@Nullable T t);
-	@Nullable T get();
-}
